5604bd1cb4e5e848d97bcbf97556f9acfb3e9990
iamdumonde <128897874+iamdumonde@users.noreply.github.com>
2023-07-23 12:59:30 +0100

    new add

---
+++ src/app/album.ts
@@ -0,0 +1,16 @@
+export class Album {
+    constructor(
+      public id:string,
+      public ref:string,
+      public name:string,
+      public title:string,
+      public description:string,
+      public duration:number,
+      public status:string,
+      public like?:string,
+      public url?:string,
+      public tags?:string[]
+    ){}
+}
+
+export class List {}
--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Importew la definition de la classe et les albums
+import { Album } from '../album';
+import { ALBUMS } from '../mock-albums';
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.css']
 })
-export class AlbumsComponent {
+export class AlbumsComponent implements OnInit {
+  titlePage: string = "Page principale Albums Music";
+  albums: Album[] = ALBUMS;
+  constructor(){}
 
+  ngOnInit(): void {
+      
+  }
 }
+
