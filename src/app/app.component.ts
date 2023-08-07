5e3c150efff2acfca3848adfd4b79b3bdf6c2429
iamdumonde <martinaboumonde@gmail.com>
2023-08-07 10:51:37 +0100

    Make a Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, Input, numberAttribute, OnInit } from '@angular/core';
+import { interval, Observable, map, take } from 'rxjs';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-music';
   receivedText: string | undefined;
+  
+  // timers
+  timerObservable!: Observable<string>;
+  count!:string;//opérateur de confiance | opérateur d'affirmation de non nullité
+  //le Math floor permet de ne s'interesser à la partie décimale
+
+  ngOnInit(): void {
+   this.timerObservable = interval(1000).pipe(
+    take(3600 * 12),
+    map(num => {
+      const hours = Math.floor(num / 3600);
+      const minutes = Math.floor(num / 60)
+      return `${this.format(hours)}h ${this.format(minutes - hours * 60)}min ${this.format(num - minutes * 60)}s`
+    })
+
+   );
+   this.timerObservable.subscribe(num => {
+     this.count = num;
+    })
+  }
+
+  //méthode using in ngOnit to display the timers
+  format(num: number): string {
+   return (num < 10 ? '0' : '') + num;
+  }
 
   parentReceive($event: string | undefined){
     this.receivedText = $event;
